b9938303b4691a057432951c3678f55d3b709b82
Sofia Leyton <adrianasofialeytoncaperra@gmail.com>
2026-02-17 08:42:12 -0500

    validacion ttrabajo en clase

---
+++ validacion.js
@@ -0,0 +1,21 @@
+let usuario = document.getElementById("usuario");
+
+usuario.addEventListener("input", function(){
+        this.value = this.value.toLowerCase();
+        this.value = this.value.replace(/[^a-z]/g, "")
+        this.border = "2px solid"
+        if(this.value == ""){
+            this.style.borderColor = "red"
+        }else{
+            this.style.borderColor = "green"
+            this.nextElementSibling && (this.nextElementSibling.textContent = 'Nombre válido');
+    this.nextElementSibling && (this.nextElementSibling.style.color = 'green');
+        }
+ }
+);
+
+//     if(evento.keyCode === 8) {
+//         evento.preventDefault()
+//         this.value += "borrando"
+//     }
+// })
